7da3f94d544034e66f09c37b7680c40fcde9cac5
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-03-19 01:58:16 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# setup-flox
+
+Download, install, and setup [Flox](https://floxdev.com/) in GitHub Actions.
+
+## 🚀 Usage
+
+```yaml
+- uses: tsirysndr/setup-flox@v1
+```
+
+See [action.yml](action.yml) for the full documentation for this action's inputs and outputs.
+
+## 📝 License
+[MIT](LICENSE)
---
+++ dist/action.js
@@ -0,0 +1,14 @@
+import { tmpdir } from "node:os";
+import * as action from "@actions/core";
+import setup from "./setup.js";
+if (!process.env.RUNNER_TEMP) {
+    process.env.RUNNER_TEMP = tmpdir();
+}
+setup()
+    .then(({ nixVersion, floxVersion }) => {
+    action.setOutput("nix-version", nixVersion);
+    action.setOutput("flox-version", floxVersion);
+})
+    .catch((error) => {
+    action.setFailed(error.message);
+});
